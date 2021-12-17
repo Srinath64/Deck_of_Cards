1815b00b24785dd160729285c79520a3fb74f26d
Srinath S <srinath@Srinaths-MacBook-Air.local>
2021-12-17 10:41:15 +0530

    First Commit

---
+++ DeckMain.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class DeckMain {
+    public static void main(String[] args) {
+        DeckOfCards card = new DeckOfCards();
+
+        card.cardCreation();
+        card.shuffle();
+        card.distribute();
+    }
+}
